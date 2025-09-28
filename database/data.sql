ab5ded5ae7a816001e67e1c5f3efc5d085dacda7
DQ-Bao <doanqbao2506@gmail.com>
2025-09-28 14:52:40 +0700

    update db schema

---
+++ database/data.sql
@@ -0,0 +1,4 @@
+insert into roles ("name") values ("Admin"), ("Technician");
+
+insert into users ("fullname", "phone", "password_hash", "role_id")
+values ("Doãn Quốc Bảo", "0382633428", "AQAAAAIAAYagAAAAEC7iGEcwGcYC51eb2ijKCRyIa18U40iGykiY27MJ06+6UzKwx/heauSLbMSeFifZag==", 1);
