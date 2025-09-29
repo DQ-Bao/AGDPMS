09b6058577fbc390c9cc68af1fa737abb4d37278
DM-Duy <no29082004@gmail.com>
2025-09-29 22:54:24 +0700

    Add material profiles to database

--- database/table.sql
+++ database/table.sql
@@ -1,3 +1,15 @@
+SET CLIENT_ENCODING TO 'utf8';
+/*
+DROP TABLE IF EXISTS takeoff;
+DROP TABLE IF EXISTS inventory_receipts;
+DROP TABLE IF EXISTS glass;
+DROP TABLE IF EXISTS projects;
+*/
+
+DROP TABLE IF EXISTS profiles;
+DROP TABLE IF EXISTS users;
+drop table if exists roles;
+
 create table if not exists roles ( 
   "id" serial,
   "name" varchar(250) not null,
@@ -17,3 +29,34 @@ create table if not exists users (
   constraint "FK_users_role_id" foreign key ("role_id") references roles ("id"),
   constraint "UQ_users_phone" unique ("phone")
 );
+
+CREATE TABLE IF NOT EXISTS profiles (
+	"profile_id" varchar(250),
+	"profile_name" varchar(250),
+	"linear_density" numeric(10,3),
+	PRIMARY KEY ("profile_id")
+)
+/*
+CREATE TABLE IF NOT EXISTS projects (
+	project_id varchar,
+	PRIMARY KEY (project_id)
+);
+
+CREATE TABLE IF NOT EXISTS glass (
+	glass_id varchar,
+	glass_name varchar,
+	PRIMARY KEY (glass_id)
+)
+
+CREATE TABLE IF NOT EXISTS inventory_receipts (
+    receipt_id varchar,
+	PRIMARY KEY (receipt_id)
+);
+
+CREATE TABLE IF NOT EXISTS takeoff (
+	project_id varchar,
+	material_id varchar references materials(material_id),
+	item_count int,
+	PRIMARY KEY (project_id)
+);
+*/
