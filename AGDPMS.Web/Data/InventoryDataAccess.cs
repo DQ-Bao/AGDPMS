5a8cdfe4801c592116b2bb56306c808549591307
DM-Duy <no29082004@gmail.com>
2025-10-05 17:06:20 +0700

    Add CRUD for glass and accessory

--- AGDPMS.Web/Data/InventoryDataAccess.cs
+++ AGDPMS.Web/Data/InventoryDataAccess.cs
@@ -27,7 +27,7 @@ public class InventoryDataAccess(IDbConnection conn)
             a.linear_density as LinearDensity,
             a.quantity as Quantity
         from aluminum a
-        where a.id = @Id;";
+        where a.aluminum_id = @Id;";
         return (await conn.QueryAsync<Aluminum>(query, new { Id = id })).FirstOrDefault();
     }
 
@@ -62,5 +62,119 @@ public class InventoryDataAccess(IDbConnection conn)
             }
         );
         return aluminum;
+    }
+
+    public Task<IEnumerable<Glass>> GetAllGlassAsync()
+    {
+        string query = @"
+        select
+            g.glass_id as Id,
+            g.glass_name as Name,
+            g.thickness as Thickness,
+            g.quantity as Quantity
+        from glass g;";
+
+        return conn.QueryAsync<Glass>(query);
+    }
+
+    public async Task<Glass?> GetGlassByIdAsync(string id)
+    {
+        string query = @"
+        select
+            g.glass_id as Id,
+            g.glass_name as Name,
+            g.thickness as Thickness,
+            g.quantity as Quantity
+        from glass g
+        where g.glass_id = @Id;";
+        return (await conn.QueryAsync<Glass>(query, new { Id = id })).FirstOrDefault();
+    }
+
+    public Task<IEnumerable<Glass>> GetGlassByNameAsync(string name)
+    {
+        string query = @"
+        select
+            g.glass_id as Id,
+            g.glass_name as Name,
+            g.thickness as Thickness,
+            g.quantity as Quantity
+        from glass g
+        where g.glass_name like '%@Name%';";
+        return conn.QueryAsync<Glass>(query, new { Name = name });
+    }
+
+    public async Task<Glass> CreateGlassAsync(Glass glass)
+    {
+        string insert = @"
+            insert into glass(glass_id, glass_name, thickness, quantity)
+            values (@Id, @Name, @Thickness, @Quantity);
+        ";
+
+        await conn.ExecuteScalarAsync<int>(
+            insert,
+            new
+            {
+                glass.Id,
+                glass.Name,
+                glass.Thickness,
+                glass.Quantity
+            }
+        );
+        return glass;
+    }
+
+    public Task<IEnumerable<Accessory>> GetAllAccessoryAsync()
+    {
+        string query = @"
+        select
+            a.accessory_id as Id,
+            a.accessory_name as Name,
+            a.quantity as Quantity
+        from accessory a;";
+
+        return conn.QueryAsync<Accessory>(query);
+    }
+
+    public async Task<Accessory?> GetAccessoryByIdAsync(string id)
+    {
+        string query = @"
+        select
+            a.accessory_id as Id,
+            a.accessory_name as Name,
+            a.quantity as Quantity
+        from accessory a
+        where a.accessory_id = @Id;";
+        return (await conn.QueryAsync<Accessory>(query, new { Id = id })).FirstOrDefault();
+    }
+
+    public Task<IEnumerable<Accessory>> GetAccessoryByNameAsync(string name)
+    {
+        string query = @"
+        select
+            a.accessory_id as Id,
+            a.accessory_name as Name,
+            a.quantity as Quantity
+        from accessory a
+        where g.accessory_name like '%@Name%';";
+        return conn.QueryAsync<Accessory>(query, new { Name = name });
+    }
+
+    public async Task<Accessory> CreateAccessoryAsync(Accessory accessory)
+    {
+        string insert = @"
+            insert into accessory(glass_id, glass_name, quantity)
+            values (@Id, @Name, @Quantity);
+        ";
+
+        await conn.ExecuteScalarAsync<int>(
+            insert,
+            new
+            {
+                accessory.Id,
+                accessory.Name,
+                accessory.Quantity
+            }
+        );
+        return accessory;
     }
 }
