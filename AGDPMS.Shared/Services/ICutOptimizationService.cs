780ff46c4b285fb2a6dba3e31950b9d215a77dae
DM-Duy <no29082004@gmail.com>
2025-10-29 22:49:38 +0700

    Removed unused unnecessary usings

--- AGDPMS.Shared/Services/ICutOptimizationService.cs
+++ AGDPMS.Shared/Services/ICutOptimizationService.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.AccessControl;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using Google.OrTools.LinearSolver;
+﻿using Google.OrTools.LinearSolver;
 
 namespace AGDPMS.Shared.Services;
 
