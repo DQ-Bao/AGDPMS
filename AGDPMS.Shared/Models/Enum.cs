184cf8ae7fef2abb9d10d8ef1b8085e7e8a5a975
DQ-Bao <doanqbao2506@gmail.com>
2025-11-15 14:38:21 +0700

    change project status enum

--- AGDPMS.Shared/Models/Enum.cs
+++ AGDPMS.Shared/Models/Enum.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AGDPMS.Shared.Models;
+﻿namespace AGDPMS.Shared.Models;
 
 // Enum để quản lý các trạng thái
-public enum ProjectRFQStatus { Pending, Scheduled, Active, Completed }
+public enum ProjectRFQStatus { Planning, Production, Deploying, Completed, Cancelled }
 
 public enum MachineStatus
 {
